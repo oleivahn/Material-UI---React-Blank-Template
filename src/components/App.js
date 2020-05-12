3e32add5c0184a6bed777f9e51929ef00a4e68c7
Omar Leiva <oleivahn@gmail.com>
2020-05-12 15:35:22 -0400

    Fully working navigation bar with routes

--- src/components/App.js
+++ src/components/App.js
@@ -17,22 +17,22 @@ function App() {
           <Route
             exact
             path="/customsoftware"
-            component={() => <div>customsoftware</div>}
+            component={() => <div>Custom Software</div>}
           />
           <Route
             exact
             path="/mobileapps"
-            component={() => <div>mobileapps</div>}
+            component={() => <div>Mobile Apps</div>}
           />
           <Route exact path="/websites" component={() => <div>websites</div>} />
           <Route
             exact
             path="/revolution"
-            component={() => <div>revolution</div>}
+            component={() => <div>Revolution</div>}
           />
-          <Route exact path="/about" component={() => <div>about</div>} />
-          <Route exact path="/contact" component={() => <div>contact</div>} />
-          <Route exact path="/estimate" component={() => <div>estimate</div>} />
+          <Route exact path="/about" component={() => <div>About Us</div>} />
+          <Route exact path="/contact" component={() => <div>Contact</div>} />
+          <Route exact path="/estimate" component={() => <div>Estimate</div>} />
           <Route component={() => <div>NOT FOUND</div>} />
         </Switch>
       </BrowserRouter>
