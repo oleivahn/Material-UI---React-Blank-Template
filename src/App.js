33be0654730f757261de78f8ba36f2305a07ba9b
Omar Leiva <oleivahn@gmail.com>
2020-05-12 10:26:17 -0400

    Blank template first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function App() {
-  return <div className="App">Hello!</div>;
+  return (
+    <div className="App">
+      Hello, this is the blank template of a Material-UI and React application
+    </div>
+  );
 }
 
 export default App;
